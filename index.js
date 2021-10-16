cf985f3c36d8a46f3e315f716a873af2784c9238
vha04 <vuhoanganh.dev@gmail.com>
2021-10-16 10:05:14 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+
+const http = require('http')
+
+const server = http.createServer(app)
+
+const {Server} = require('socket.io')
+
+const io = new Server(server)
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+
+})
+app.get('/test', (req, res) => {
+  res.sendFile(__dirname + '/test.html')
+
+})
+var dem = 1
+
+io.on('connection', (socket) => {
+	
+	var ip = socket.client.request.headers['x-forwarded-for'] || socket.client.conn.remoteAddress || socket.conn.remoteAddress || socket.request.connection.remoteAddress;
+	console.log(ip)
+	console.log('user ket noi ' + dem)
+	dem += 1
+	socket.on('on-chat', data => {
+			io.emit('user-chat', data)
+	})
+}) 
+
+
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+
+
+})
